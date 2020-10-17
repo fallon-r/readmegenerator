e3ea9a624eb061b0af2e72da4b91f198ee7b978d
rf-spuds <rfspuds@gmail.com>
2020-10-16 21:19:07 -0400

    Final commit with clean .gitignore, completed readme and proofread code.

--- index.js
+++ index.js
@@ -3,77 +3,95 @@ const inquirer = require("inquirer");
 const axios = require("axios");
 
 // These are the questions it will ask when you run the app
-inquirer.prompt([
+inquirer
+  .prompt([
     {
-        type: "input",
-        name: "github",
-        message: "What is your Github username?",
+      type: "input",
+      name: "github",
+      message: "What is your Github username?",
     },
     {
-        type: "input",
-        name: "email",
-        message: "What is your email?",
+      type: "input",
+      name: "email",
+      message: "What is your email?",
     },
     {
-        type: "input",
-        name: "url",
-        message: "What is the URL to your project?",
+      type: "input",
+      name: "url",
+      message: "What is the URL to your project?",
     },
     {
-        type: "input",
-        name: "title",
-        message: "What is your project's name?",
+      type: "input",
+      name: "title",
+      message: "What is your project's name?",
     },
     {
-        type: "input",
-        name: "description",
-        message: "Please write a short description of your project",
+      type: "input",
+      name: "description",
+      message: "Please write a short description of your project",
     },
     {
-        type: "list",
-        name: "license",
-        message: "What kind of license should your project have?",
-        choices: ["MIT", "APACHE", "GPL", "BSD", "None"],
+      type: "list",
+      name: "license",
+      message: "What kind of license should your project have?",
+      choices: ["MIT", "APACHE", "GPL", "BSD", "None"],
     },
     {
-        type: "input",
-        name: "installation",
-        message: "What command should be run to install dependencies?",
-        default: "npm i",
+      type: "input",
+      name: "installation",
+      message: "What command should be run to install dependencies?",
+      default: "npm i",
     },
     {
-        type: "input",
-        name: "test",
-        message: "What command should be run to run tests?",
-        default: "npm test",
+      type: "input",
+      name: "test",
+      message: "What command should be run to run tests?",
+      default: "npm test",
     },
     {
-        type: "input",
-        name: "usage",
-        message: "What does the user need to know about using the repo?",
+      type: "input",
+      name: "usage",
+      message: "What does the user need to know about using the repo?",
     },
     {
-        type: "input",
-        name: "contributing",
-        message: "What does the user need to know about contributing to the repo?",
+      type: "input",
+      name: "contributing",
+      message:
+        "What does the user need to know about contributing to the repo?",
     },
-]).then((response) => {
-
-    const githubCall = `https://api.github.com/users/${response.github}`
+  ])
+  .then((response) => {
+    const githubCall = `https://api.github.com/users/${response.github}`;
 
     axios.get(githubCall).then((res) => {
-        var avatar = res.data.avatar_url;
-        var name = res.data.name;
-        var githubPage = res.data.html_url;
+      var avatar = res.data.avatar_url;
+      var name = res.data.name;
+      var githubPage = res.data.html_url;
 
-        const data = dataDump(response, avatar, name, githubPage);
+      const data = dataDump(response, avatar, name, githubPage);
 
-        fs.writeFile("README.md", data, function () { })
-    })
-});
+      fs.writeFile("README.md", data, function () {});
+    });
+  });
 
-const dataDump = ({github, url, email, title, description, license, installation, test, usage, contributing}, avatar, name, githubPage) =>{ 
-    return `
+const dataDump = (
+  {
+    github,
+    url,
+    email,
+    title,
+    description,
+    license,
+    installation,
+    test,
+    usage,
+    contributing,
+  },
+  avatar,
+  name,
+  githubPage
+) => {
+  return `
 # ${title}
 
 ## Description
